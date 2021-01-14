d575fd6040a66bbacb99021ee9b2a6dc3dc367c3
refaelok <refaeloknin@gmail.com>
2021-01-14 22:54:27 +0200

    - use mock

--- src/actions/sagas/catalog/index.ts
+++ src/actions/sagas/catalog/index.ts
@@ -3,20 +3,10 @@ import {
 } from 'redux-saga/effects';
 import { AxiosResponse } from 'axios';
 import CatalogActions, { CatalogTypes } from 'actions/redux/catalog';
-import { Device } from 'actions/redux/catalog/interfaces';
-import GenericMobileImage from './generic-mobile.jpg';
 import { startFlow } from '../flowManager';
 import { TypesNames } from 'actions/redux/flowManager/interfaces';
 import { FlowTypes, StepTypes } from 'configurations/flows.steps.types';
 import { AppContextProps } from '@base/features/base-context';
-
-const getDevicePrice = (device: any): number => {
-	if (device.price && device.price.includes('&#36;')) {
-		return parseFloat(device.price.split('/')[0].split(';')[2].replace(',', ''));
-	}
-
-	return Math.floor((Math.random() * 1000) + 1);
-};
 
 function* getDevices(action: any & AppContextProps) {
 	try {
@@ -29,23 +19,9 @@ function* getDevices(action: any & AppContextProps) {
 
 		const { api } = action.applicationDetails;
 		const response: AxiosResponse = yield call(api.getDevicesMock);
-		const genericImage = GenericMobileImage;
 
 		if (response?.data) {
-			const deviceList: Device[] = response.data.map((device: any): Device => {
-				const newDevice: Device = {
-					id: device.DeviceName,
-					name: device.DeviceName,
-					price: getDevicePrice(device),
-					description: `${device.type} ${device.status}`,
-					brand: device.Brand,
-					image: genericImage
-				};
-
-				return newDevice;
-			});
-
-			yield put(CatalogActions.setDeviceList(deviceList));
+			yield put(CatalogActions.setDeviceList(response.data));
 		}
 	} catch (e) {
 		// eslint-disable-next-line no-console
