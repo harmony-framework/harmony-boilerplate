20367b7403615b94326a344589b4d4e5049d4162
refaelok <refaeloknin@gmail.com>
2021-01-27 00:12:27 +0200

    - add types to base connect

--- src/base/features/base-redux-react-connect/index.ts
+++ src/base/features/base-redux-react-connect/index.ts
@@ -3,19 +3,24 @@ import { withRouter } from 'react-router-dom';
 import { withLocalize } from 'react-localize-redux';
 import { reduxForm } from 'redux-form';
 
-export function baseConnect(component: any, mapStateToProps: any, propsToDispatch: any) {
-	return withRouter(connect(mapStateToProps, propsToDispatch)(withLocalize(component)));
+export function baseConnect<S, D, O>(component: any, mapStateToProps: any, propsToDispatch: any) {
+	const componentWithRouter = withRouter(component);
+	return (
+		connect<S, D, O>(
+			mapStateToProps, propsToDispatch
+		)(withLocalize<any>(componentWithRouter))
+	);
 }
 
-export function baseConnectForm(component: any, mapStateToProps: any, propsToDispatch: any, formConfig: any) {
-	return connectWithReduxForm(baseConnect(component, mapStateToProps, propsToDispatch), formConfig);
+export function baseConnectForm<S, D, O>(component: any, mapStateToProps: any, propsToDispatch: any, formConfig: any) {
+	return connectWithReduxForm<S, D, O>(baseConnect<S, D, O>(component, mapStateToProps, propsToDispatch), formConfig);
 }
 
-function connectWithReduxForm(component: any, reduxFormConfig: any) {
+function connectWithReduxForm<S, D, O>(component: any, reduxFormConfig: any) {
 	const defaultValidateFunction = () => undefined;
 
 	// eslint-disable-next-line no-param-reassign
 	reduxFormConfig.validate = component.prototype.validate || defaultValidateFunction;
 
-	return reduxForm(reduxFormConfig)(component);
+	return reduxForm<S, O>(reduxFormConfig)(component);
 }
