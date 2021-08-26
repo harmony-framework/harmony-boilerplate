533f847006979e2fd1a9253950ea239040e9668a
refaelok <refaeloknin@gmail.com>
2021-08-26 23:05:36 +0300

    - Auto Done action for every saga and reducer

--- src/base/features/base-decorator/createReducerCase.ts
+++ src/base/features/base-decorator/createReducerCase.ts
@@ -1,14 +1,15 @@
 import produce from 'immer';
 import { Store } from '@base/features';
 
-export default (originalReducerCase: Function) => {
+export default (originalReducerCase: Function, isDoneHandler = false) => {
 	return produce((...args: any) => {
 		const type = args?.[1]?.type;
 		const payload = { ...args?.[1] };
 
 		const res = originalReducerCase(...args);
 
-		if (type) {
+		if (type && !isDoneHandler) {
+			// settimeout to push action to last in memory stack
 			setTimeout(() => {
 				Store.dispatch({ type: `${type}_DONE`, payload });
 			}, 0);
--- src/base/features/base-decorator/createSaga.ts
+++ src/base/features/base-decorator/createSaga.ts
@@ -1,13 +1,13 @@
 import { put } from 'redux-saga/effects';
 
-export default (originalSaga: Function) => {
+export default (originalSaga: Function, isDoneHandler = false) => {
 	return function* (...args: any) {
 		const type = args?.[0]?.type;
 		const payload = { ...args?.[0] };
 
 		const response = yield originalSaga(...args);
 
-		if (type) {
+		if (type && !isDoneHandler) {
 			yield put({ type: `${type}_DONE`, response, payload });
 		}
 
