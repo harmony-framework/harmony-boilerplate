09f98cc287ff1d108e6364a6a10bbe66c72f874d
refaelok <refaeloknin@gmail.com>
2021-09-11 16:16:56 +0300

    - seprate webpack to mobile

--- mobile-app/webpack.config.js
+++ mobile-app/webpack.config.js
@@ -1,9 +1,12 @@
 const isProduction = process.env.NODE_ENV === "production";
 const isMobileApp = process.env.BUILD_TYPE === "mobile";
+const platform = process.env.npm_config_platform;
+
+const devPublicPath = (platform === "android") ? "http://10.0.2.2:8082/" : "http://localhost:8082/";
 
 const mobileConfig = {
 	output: {
-		publicPath: (isProduction) ? "/" : "http://10.0.2.2:8082/",
+		publicPath: (isProduction) ? "/" : devPublicPath,
 	},
 	devServer: {
 		host: '0.0.0.0'
