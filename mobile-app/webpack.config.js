61bdc441cfdea713b596c95e5b8021a2de6fc1fb
refaelok <refaeloknin@gmail.com>
2021-09-19 15:16:35 +0300

    - enable to use cordova in dev mode with live reload

--- mobile-app/webpack.config.js
+++ mobile-app/webpack.config.js
@@ -1,10 +1,12 @@
 const execSh = require("exec-sh");
+const HtmlReplaceWebpackPlugin = require('html-replace-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV === "production";
 const isMobileApp = process.env.BUILD_TYPE === "mobile";
 const platform = process.env.npm_config_platform;
 
 const devPublicPath = (platform === "android") ? "http://10.0.2.2:8082/" : "http://localhost:8082/";
+const cordovaPath = (platform === "android") ? "/mobile-app/platforms/android/app/src/main/assets/www/cordova.js" : "/mobile-app/platforms/ios/platform_www/cordova.js";
 let isDone = false;
 
 if (isMobileApp && !platform) {
@@ -19,6 +21,10 @@ const mobileConfig = {
 		host: '0.0.0.0'
 	},
 	plugins: [
+		new HtmlReplaceWebpackPlugin([{
+			pattern: '${cordovaPath}',
+			replacement: (isProduction) ? '/cordova.js' : cordovaPath,
+		}]),
 		{
 			apply: compiler => {
 				compiler.hooks.done.tap('run-cordova', compilation => {
