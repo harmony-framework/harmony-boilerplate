cc6857638b4b73d17eb983d7fce47e6e157fde0b
refaelok <refaeloknin@gmail.com>
2021-11-04 23:16:51 +0200

    - upgrade storybook

--- babel.config.js
+++ babel.config.js
@@ -8,6 +8,8 @@ module.exports = {
 		"react-require",
 		"react-hot-loader/babel",
 		["@babel/plugin-proposal-decorators" ,{legacy: true }],
-		["@babel/plugin-proposal-class-properties", { loose : true}]
+		["@babel/plugin-proposal-class-properties", { loose : true}],
+		["@babel/plugin-proposal-private-methods", { loose: true }],
+		["@babel/plugin-proposal-private-property-in-object", {loose: true }]
 	]
 };
